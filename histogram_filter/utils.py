7c3a21f0c41c4a4c838304dfd551fbf3a3b07682
Ludwig Stumpp <42147848+LudwigStumpp@users.noreply.github.com>
2022-03-30 22:06:21 +0200

    Factor out additional helpers

--- histogram_filter/utils.py
+++ histogram_filter/utils.py
@@ -1,4 +1,6 @@
 import numpy as np
+from world import Grid, Color, Agent, Environment, TransitionSpecification
+import termplotlib as tpl
 
 
 def bayes(prior: np.ndarray, conditional: np.ndarray) -> np.ndarray:
@@ -33,3 +35,39 @@ def sense(curr_belief: np.ndarray, measurement_idx: int, emission_matrix: np.nda
 
 def propagate(curr_belief: np.ndarray, transition_matrix: np.ndarray) -> np.ndarray:
     return transition_matrix @ curr_belief
+
+
+def setup(
+    correct_measurement_prob: float = 0.9,
+    transition: TransitionSpecification = TransitionSpecification(0, [0.05, 0.9, 0.05]),
+    start_position: int = 0,
+) -> Environment:
+    """Sets up the environment for the histogram filter.
+
+    Args:
+        correct_measurement_prob (float, optional): Sensor accuracy. Defaults to 0.9.
+        transition (TransitionSpecification, optional): Transition probabilities. Defaults to
+            TransitionSpecification(0, [0.05, 0.9, 0.05]).
+        start_position (int, optional): Start index on the grid. Defaults to 0.
+
+    Returns:
+        Environment: Environment object.
+    """
+    field = [Color.BLUE, Color.ORANGE, Color.BLUE, Color.BLUE, Color.ORANGE]
+    my_grid = Grid(field)
+    transition = transition
+    my_agent = Agent(transition=transition, correct_measurement_prob=correct_measurement_prob)
+    my_env = Environment(my_grid, my_agent, start_position=start_position)
+    return my_env
+
+
+def visualize_belief(belief: np.ndarray):
+    """Visualizes the belief as a line plot.
+
+    Args:
+        belief (np.ndarray): 1D array of shape (N,) of belief probabilities.
+            Number of entries equals the number of states.
+    """
+    fig = tpl.figure()
+    fig.plot(np.arange(1, len(belief) + 1), belief, width=50, height=15)
+    fig.show()
